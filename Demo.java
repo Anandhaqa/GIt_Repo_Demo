e125ecf64ed4ec67c4e7ac4f1012280f394891c2
Anandhakrishnan <140926589+Anandhaqa@users.noreply.github.com>
2026-07-01 11:07:50 +0530

    Feat: New Method Added

--- Demo.java
+++ Demo.java
@@ -2,6 +2,11 @@ public class Demo{
     public void method1(){
         System.out.println("Method1 Called");
     }
+
+    public void method2(){
+        System.out.println("Method2 Called");
+    }
+    
     public static void main(String[] args) {
         System.out.println("Verify");
         Demo dd = new Demo();
