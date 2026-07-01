5362ad91e8e95bdf75076b64215154fc3bd058b8
Anandhaqa <anandhaqa@gmail.com>
2026-07-01 11:04:50 +0530

    feat: Added Method in Demo.java

--- Demo.java
+++ Demo.java
@@ -1,4 +1,7 @@
 public class Demo{
+    public void method1(){
+        System.out.println("Method1 Called");
+    }
     public static void main(String[] args) {
         System.out.println("Verify");
     }
